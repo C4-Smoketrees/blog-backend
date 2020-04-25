97635c60373b3f3803513aa6cb4cc3166561b0aa
Anshuman <achhap.10.01@gmail.com>
2020-04-25 11:44:51 +0530

    Add blog search route
    Signed-off-by: Anshuman <achhap.10.01@gmail.com>

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,145 +1,162 @@
-const router = require('express').Router();
-const bson = require('bson');
+const router = require('express').Router()
+const bson = require('bson')
 
-const jwtAuth = require('../middleware/jwtAuth');
-const jwtUnAuth = require('../middleware/jwtUnAuth');
-const User = require('../modules/users/model');
-const Blogs = require('../modules/blogs/model');
+const jwtAuth = require('../middleware/jwtAuth')
+const jwtUnAuth = require('../middleware/jwtUnAuth')
+const User = require('../modules/users/model')
+const Blogs = require('../modules/blogs/model')
 
 router.get('/one', jwtUnAuth, async (req, res) => {
-  const userId = req.userId;
-  const blogId = req.query.blogId;
-  const response = await Blogs.readBlogUsingId(blogId, req.app.locals.blogCollection, userId);
+  const userId = req.userId
+  const blogId = req.query.blogId
+  const response = await Blogs.readBlogUsingId(blogId, req.app.locals.blogCollection, userId)
   if (response.status) {
-    res.status(200).json(response);
+    res.status(200).json(response)
   } else if (response.err) {
-    res.status(500).json(response);
+    res.status(500).json(response)
   } else {
-    res.status(400).json(response);
+    res.status(400).json(response)
   }
-});
+})
 
 router.get('/all', jwtUnAuth, async (req, res) => {
-  const userId = req.userId;
-  const response = await Blogs.readAllBlogs(req.app.locals.blogCollection, userId);
+  const userId = req.userId
+  const response = await Blogs.readAllBlogs(req.app.locals.blogCollection, userId)
   if (response.status) {
-    res.status(200).json(response);
+    res.status(200).json(response)
   } else if (response.err) {
-    res.status(500).json(response);
+    res.status(500).json(response)
   } else {
-    res.status(400).json(response);
+    res.status(400).json(response)
   }
-});
+})
 
 router.post('/delete', jwtAuth, async (req, res) => {
-  const userId = req.userId;
-  const blogId = req.body._id;
-  const user = new User({ _id: bson.ObjectID.createFromHexString(userId) });
-  const response = await user.deleteBlog(blogId, req.app.locals.userCollection, req.app.locals.blogCollection);
+  const userId = req.userId
+  const blogId = req.body._id
+  const user = new User({ _id: bson.ObjectID.createFromHexString(userId) })
+  const response = await user.deleteBlog(blogId, req.app.locals.userCollection, req.app.locals.blogCollection)
   if (response.status) {
-    res.status(200).json(response);
+    res.status(200).json(response)
   } else if (response.err) {
-    res.status(500).json(response);
+    res.status(500).json(response)
   } else {
-    res.status(400).json(response);
+    res.status(400).json(response)
   }
-});
+})
 
 router.post('/update', jwtAuth, async (req, res) => {
-  const userId = req.userId;
-  const blog = req.body.blog;
-  const user = new User({ _id: bson.ObjectID.createFromHexString(userId) });
-  blog._id = bson.ObjectID.createFromHexString(blog._id);
-  const response = await Blogs.updateBlogContent(blog, user._id.toHexString(), req.app.locals.blogCollection);
+  const userId = req.userId
+  const blog = req.body.blog
+  const user = new User({ _id: bson.ObjectID.createFromHexString(userId) })
+  blog._id = bson.ObjectID.createFromHexString(blog._id)
+  const response = await Blogs.updateBlogContent(blog, user._id.toHexString(), req.app.locals.blogCollection)
   if (response.status) {
-    res.status(200).json(response);
+    res.status(200).json(response)
   } else if (response.err) {
-    res.status(500).json(response);
+    res.status(500).json(response)
   } else {
-    res.status(400).json(response);
+    res.status(400).json(response)
   }
-});
+})
 
 router.post('/star', jwtAuth, async (req, res) => {
-  const userId = req.userId;
-  const blogId = req.body.blogId;
-  const user = new User({ _id: bson.ObjectID.createFromHexString(userId) });
-  const response = await user.addStar(blogId, req.app.locals.userCollection, req.app.locals.blogCollection);
+  const userId = req.userId
+  const blogId = req.body.blogId
+  const user = new User({ _id: bson.ObjectID.createFromHexString(userId) })
+  const response = await user.addStar(blogId, req.app.locals.userCollection, req.app.locals.blogCollection)
   if (response.status) {
-    res.status(200).json(response);
+    res.status(200).json(response)
   } else if (response.err) {
-    res.status(500).json(response);
+    res.status(500).json(response)
   } else {
-    res.status(400).json(response);
+    res.status(400).json(response)
   }
-});
+})
 
 router.post('/unstar', jwtAuth, async (req, res) => {
-  const userId = req.userId;
-  const blogId = req.body.blogId;
-  const user = new User({ _id: bson.ObjectID.createFromHexString(userId) });
-  const response = await user.removeStar(blogId, req.app.locals.userCollection, req.app.locals.blogCollection);
+  const userId = req.userId
+  const blogId = req.body.blogId
+  const user = new User({ _id: bson.ObjectID.createFromHexString(userId) })
+  const response = await user.removeStar(blogId, req.app.locals.userCollection, req.app.locals.blogCollection)
   if (response.status) {
-    res.status(200).json(response);
+    res.status(200).json(response)
   } else if (response.err) {
-    res.status(500).json(response);
+    res.status(500).json(response)
   } else {
-    res.status(400).json(response);
+    res.status(400).json(response)
   }
-});
+})
 
 router.post('/upvote', jwtAuth, async (req, res) => {
-  const userId = req.userId;
-  const blogId = req.body.blogId;
-  await Blogs.removeDownvote(blogId, userId, req.app.locals.blogCollection);
-  const response = await Blogs.addUpvote(blogId, userId, req.app.locals.blogCollection);
+  const userId = req.userId
+  const blogId = req.body.blogId
+  await Blogs.removeDownvote(blogId, userId, req.app.locals.blogCollection)
+  const response = await Blogs.addUpvote(blogId, userId, req.app.locals.blogCollection)
   if (response.status) {
-    res.status(200).json(response);
+    res.status(200).json(response)
   } else if (response.err) {
-    res.status(500).json(response);
+    res.status(500).json(response)
   } else {
-    res.status(400).json(response);
+    res.status(400).json(response)
   }
-});
+})
 
 router.post('/downvote', jwtAuth, async (req, res) => {
-  const userId = req.userId;
-  const blogId = req.body.blogId;
-  await Blogs.removeUpvote(blogId, userId, req.app.locals.blogCollection);
-  const response = await Blogs.addDownvote(blogId, userId, req.app.locals.blogCollection);
+  const userId = req.userId
+  const blogId = req.body.blogId
+  await Blogs.removeUpvote(blogId, userId, req.app.locals.blogCollection)
+  const response = await Blogs.addDownvote(blogId, userId, req.app.locals.blogCollection)
   if (response.status) {
-    res.status(200).json(response);
+    res.status(200).json(response)
   } else if (response.err) {
-    res.status(500).json(response);
+    res.status(500).json(response)
   } else {
-    res.status(400).json(response);
+    res.status(400).json(response)
   }
-});
+})
 
 router.post('/removeUpvote', jwtAuth, async (req, res) => {
-  const userId = req.userId;
-  const blogId = req.body.blogId;
-  const response = await Blogs.removeUpvote(blogId, userId, req.app.locals.blogCollection);
+  const userId = req.userId
+  const blogId = req.body.blogId
+  const response = await Blogs.removeUpvote(blogId, userId, req.app.locals.blogCollection)
   if (response.status) {
-    res.status(200).json(response);
+    res.status(200).json(response)
   } else if (response.err) {
-    res.status(500).json(response);
+    res.status(500).json(response)
   } else {
-    res.status(400).json(response);
+    res.status(400).json(response)
   }
-});
+})
 
 router.post('/removeDownvote', jwtAuth, async (req, res) => {
-  const userId = req.userId;
-  const blogId = req.body.blogId;
-  const response = await Blogs.removeDownvote(blogId, userId, req.app.locals.blogCollection);
+  const userId = req.userId
+  const blogId = req.body.blogId
+  const response = await Blogs.removeDownvote(blogId, userId, req.app.locals.blogCollection)
   if (response.status) {
-    res.status(200).json(response);
+    res.status(200).json(response)
   } else if (response.err) {
-    res.status(500).json(response);
+    res.status(500).json(response)
   } else {
-    res.status(400).json(response);
+    res.status(400).json(response)
   }
-});
+})
 
-module.exports = router;
+router.get('/search', jwtUnAuth, async (req, res) => {
+  const query = req.query.q
+
+  const lastDate = req.query.ld || Date.now()
+
+  const userId = req.userId
+  const response = await Blogs.search(query, lastDate, req.app.locals.blogCollection, userId)
+
+  if (response.status) {
+    res.status(200).json(response)
+  } else if (response.err) {
+    res.status(500).json(response)
+  } else {
+    res.status(400).json(response)
+  }
+})
+
+module.exports = router
--- test/blogs.routes.test.js
+++ test/blogs.routes.test.js
@@ -1,220 +1,225 @@
-const app = require('../app.js');
-const chai = require('chai');
-const assert = chai.assert;
-const bson = require('bson');
-const chaiHttp = require('chai-http');
-const { describe, it } = require('mocha');
-const Blog = require('../modules/blogs/model');
-chai.use(chaiHttp);
-const User = require('../modules/users/model');
-const jwt = require('jsonwebtoken');
+const app = require('../app.js')
+const chai = require('chai')
+const assert = chai.assert
+const bson = require('bson')
+const chaiHttp = require('chai-http')
+const { describe, it } = require('mocha')
+const Blog = require('../modules/blogs/model')
+chai.use(chaiHttp)
+const User = require('../modules/users/model')
+const jwt = require('jsonwebtoken')
 
 describe('# Route test for /blogs', function () {
   it('GET /one', async function () {
-    let res;
+    let res
     try {
-      const author = new bson.ObjectId(bson.ObjectId.generate());
+      const author = new bson.ObjectId(bson.ObjectId.generate())
       const response = await Blog.createBlog({
         author: author,
         title: 'Title',
         content: 'route blog',
         tags: ['route', 'get', 'one', 'blog']
-      }, app.locals.blogCollection);
+      }, app.locals.blogCollection)
 
       res = await chai.request(app)
         .get(`/blogs/one?blogId=${response.blogId}`)
-        .send();
+        .send()
     } catch (e) {
-      assert.isNull(e);
+      assert.isNull(e)
     }
-    assert.equal(res.status, 200);
-  });
+    assert.equal(res.status, 200)
+  })
 
   it('GET /all', async function () {
-    let res;
+    let res
     try {
-      const author = new bson.ObjectId(bson.ObjectId.generate());
+      const author = new bson.ObjectId(bson.ObjectId.generate())
       const response = await Blog.createBlog({
         author: author,
         title: 'Title',
         content: 'route blog',
         tags: ['route', 'get', 'one', 'blog']
-      }, app.locals.blogCollection);
+      }, app.locals.blogCollection)
       res = await chai.request(app)
         .get(`/blogs/one?blogId=${response.blogId}`)
-        .send();
+        .send()
     } catch (e) {
-      assert.isNull(e);
+      assert.isNull(e)
     }
 
-    assert.equal(res.status, 200);
-  });
+    assert.equal(res.status, 200)
+  })
 
   it('POST /delete', async function () {
-    let res;
+    let res
     try {
-      const draft = { content: 'draft content', title: 'draft title', tags: ['#google', '#noob'] };
-      const token = jwt.decode('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFjaGhhcG9saWExMCIsIl9pZCI6IjVlODYxNzg0ZTg1NDY2ZmJhYmQyNTc2OCIsImlhdCI6MTU4NTg0NjI0Mn0.vJ5pQfIUX8jGSodwiKhxI9pP5HLFiko7uHUSLWeXM2k');
+      const draft = { content: 'draft content', title: 'draft title', tags: ['#google', '#noob'] }
+      const token = jwt.decode('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFjaGhhcG9saWExMCIsIl9pZCI6IjVlODYxNzg0ZTg1NDY2ZmJhYmQyNTc2OCIsImlhdCI6MTU4NTg0NjI0Mn0.vJ5pQfIUX8jGSodwiKhxI9pP5HLFiko7uHUSLWeXM2k')
 
-      const author1 = bson.ObjectID.createFromHexString(token._id);
-      const res1 = await User.createDraft(author1.toHexString(), draft, app.locals.userCollection, app.locals.tagCollection);
-      const user = new User({ _id: author1 });
-      const res2 = await user.publishDraft(res1.draftId, app.locals.userCollection, app.locals.blogCollection);
+      const author1 = bson.ObjectID.createFromHexString(token._id)
+      const res1 = await User.createDraft(author1.toHexString(), draft, app.locals.userCollection, app.locals.tagCollection)
+      const user = new User({ _id: author1 })
+      const res2 = await user.publishDraft(res1.draftId, app.locals.userCollection, app.locals.blogCollection)
       res = await chai.request(app)
         .post('/blogs/delete')
         .set({
           Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFjaGhhcG9saWExMCIsIl9pZCI6IjVlODYxNzg0ZTg1NDY2ZmJhYmQyNTc2OCIsImlhdCI6MTU4NTg0NjI0Mn0.vJ5pQfIUX8jGSodwiKhxI9pP5HLFiko7uHUSLWeXM2k',
           'Content-Type': 'application/json'
         })
-        .send({ _id: res2.blogId });
+        .send({ _id: res2.blogId })
     } catch (e) {
-      assert.isNull(e);
+      assert.isNull(e)
     }
 
-    assert.equal(res.status, 200);
-  });
+    assert.equal(res.status, 200)
+  })
 
   it('POST /update', async function () {
-    let res;
+    let res
     try {
-      const draft = { content: 'draft content', title: 'draft title', tags: ['#google', '#noob'] };
-      const token = jwt.decode('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFjaGhhcG9saWExMCIsIl9pZCI6IjVlODYxNzg0ZTg1NDY2ZmJhYmQyNTc2OCIsImlhdCI6MTU4NTg0NjI0Mn0.vJ5pQfIUX8jGSodwiKhxI9pP5HLFiko7uHUSLWeXM2k');
-      const author1 = bson.ObjectID.createFromHexString(token._id);
-      const res1 = await User.createDraft(author1.toHexString(), draft, app.locals.userCollection, app.locals.tagCollection);
-      const user = new User({ _id: author1 });
-      const res2 = await user.publishDraft(res1.draftId, app.locals.userCollection, app.locals.blogCollection);
+      const draft = { content: 'draft content', title: 'draft title', tags: ['#google', '#noob'] }
+      const token = jwt.decode('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFjaGhhcG9saWExMCIsIl9pZCI6IjVlODYxNzg0ZTg1NDY2ZmJhYmQyNTc2OCIsImlhdCI6MTU4NTg0NjI0Mn0.vJ5pQfIUX8jGSodwiKhxI9pP5HLFiko7uHUSLWeXM2k')
+      const author1 = bson.ObjectID.createFromHexString(token._id)
+      const res1 = await User.createDraft(author1.toHexString(), draft, app.locals.userCollection, app.locals.tagCollection)
+      const user = new User({ _id: author1 })
+      const res2 = await user.publishDraft(res1.draftId, app.locals.userCollection, app.locals.blogCollection)
       res = await chai.request(app)
         .post('/blogs/update')
         .set({
           Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFjaGhhcG9saWExMCIsIl9pZCI6IjVlODYxNzg0ZTg1NDY2ZmJhYmQyNTc2OCIsImlhdCI6MTU4NTg0NjI0Mn0.vJ5pQfIUX8jGSodwiKhxI9pP5HLFiko7uHUSLWeXM2k',
           'Content-Type': 'application/json'
         })
-        .send({ blog: { _id: res2.blogId, content: 'update', tags: ['update'] } });
+        .send({ blog: { _id: res2.blogId, content: 'update', tags: ['update'] } })
     } catch (e) {
-      assert.isNull(e);
+      assert.isNull(e)
     }
 
-    assert.equal(res.status, 200);
-  });
+    assert.equal(res.status, 200)
+  })
 
   it('POST /star', async function () {
-    let res;
+    let res
     try {
-      const draft = { content: 'draft content', title: 'draft title', tags: ['#google', '#noob'] };
-      const token = jwt.decode('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFjaGhhcG9saWExMCIsIl9pZCI6IjVlODYxNzg0ZTg1NDY2ZmJhYmQyNTc2OCIsImlhdCI6MTU4NTg0NjI0Mn0.vJ5pQfIUX8jGSodwiKhxI9pP5HLFiko7uHUSLWeXM2k');
+      const draft = { content: 'draft content', title: 'draft title', tags: ['#google', '#noob'] }
+      const token = jwt.decode('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFjaGhhcG9saWExMCIsIl9pZCI6IjVlODYxNzg0ZTg1NDY2ZmJhYmQyNTc2OCIsImlhdCI6MTU4NTg0NjI0Mn0.vJ5pQfIUX8jGSodwiKhxI9pP5HLFiko7uHUSLWeXM2k')
 
-      const author1 = bson.ObjectID.createFromHexString(token._id);
-      const res1 = await User.createDraft(author1.toHexString(), draft, app.locals.userCollection, app.locals.tagCollection);
-      const user = new User({ _id: author1 });
-      const res2 = await user.publishDraft(res1.draftId, app.locals.userCollection, app.locals.blogCollection);
+      const author1 = bson.ObjectID.createFromHexString(token._id)
+      const res1 = await User.createDraft(author1.toHexString(), draft, app.locals.userCollection, app.locals.tagCollection)
+      const user = new User({ _id: author1 })
+      const res2 = await user.publishDraft(res1.draftId, app.locals.userCollection, app.locals.blogCollection)
       res = await chai.request(app)
         .post('/blogs/star')
         .set({
           Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFjaGhhcG9saWExMCIsIl9pZCI6IjVlODYxNzg0ZTg1NDY2ZmJhYmQyNTc2OCIsImlhdCI6MTU4NTg0NjI0Mn0.vJ5pQfIUX8jGSodwiKhxI9pP5HLFiko7uHUSLWeXM2k',
           'Content-Type': 'application/json'
         })
-        .send({ blogId: res2.blogId });
+        .send({ blogId: res2.blogId })
     } catch (e) {
-      assert.isNull(e);
+      assert.isNull(e)
     }
-    assert.equal(res.status, 200);
-  });
+    assert.equal(res.status, 200)
+  })
 
   it('POST /upvote', async function () {
-    let res;
+    let res
     try {
-      const draft = { content: 'draft content', title: 'draft title', tags: ['#google', '#noob'] };
-      const token = jwt.decode('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFjaGhhcG9saWExMCIsIl9pZCI6IjVlODYxNzg0ZTg1NDY2ZmJhYmQyNTc2OCIsImlhdCI6MTU4NTg0NjI0Mn0.vJ5pQfIUX8jGSodwiKhxI9pP5HLFiko7uHUSLWeXM2k');
+      const draft = { content: 'draft content', title: 'draft title', tags: ['#google', '#noob'] }
+      const token = jwt.decode('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFjaGhhcG9saWExMCIsIl9pZCI6IjVlODYxNzg0ZTg1NDY2ZmJhYmQyNTc2OCIsImlhdCI6MTU4NTg0NjI0Mn0.vJ5pQfIUX8jGSodwiKhxI9pP5HLFiko7uHUSLWeXM2k')
 
-      const author1 = bson.ObjectID.createFromHexString(token._id);
-      const res1 = await User.createDraft(author1.toHexString(), draft, app.locals.userCollection, app.locals.tagCollection);
-      const user = new User({ _id: author1 });
-      const res2 = await user.publishDraft(res1.draftId, app.locals.userCollection, app.locals.blogCollection);
+      const author1 = bson.ObjectID.createFromHexString(token._id)
+      const res1 = await User.createDraft(author1.toHexString(), draft, app.locals.userCollection, app.locals.tagCollection)
+      const user = new User({ _id: author1 })
+      const res2 = await user.publishDraft(res1.draftId, app.locals.userCollection, app.locals.blogCollection)
       res = await chai.request(app)
         .post('/blogs/upvote')
         .set({
           Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFjaGhhcG9saWExMCIsIl9pZCI6IjVlODYxNzg0ZTg1NDY2ZmJhYmQyNTc2OCIsImlhdCI6MTU4NTg0NjI0Mn0.vJ5pQfIUX8jGSodwiKhxI9pP5HLFiko7uHUSLWeXM2k',
           'Content-Type': 'application/json'
         })
-        .send({ blogId: res2.blogId });
+        .send({ blogId: res2.blogId })
     } catch (e) {
-      assert.isNull(e);
+      assert.isNull(e)
     }
 
-    assert.equal(res.status, 200);
-  });
+    assert.equal(res.status, 200)
+  })
 
   it('POST /downvote', async function () {
-    let res;
+    let res
     try {
-      const draft = { content: 'draft content', title: 'draft title', tags: ['#google', '#noob'] };
-      const token = jwt.decode('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFjaGhhcG9saWExMCIsIl9pZCI6IjVlODYxNzg0ZTg1NDY2ZmJhYmQyNTc2OCIsImlhdCI6MTU4NTg0NjI0Mn0.vJ5pQfIUX8jGSodwiKhxI9pP5HLFiko7uHUSLWeXM2k');
+      const draft = { content: 'draft content', title: 'draft title', tags: ['#google', '#noob'] }
+      const token = jwt.decode('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFjaGhhcG9saWExMCIsIl9pZCI6IjVlODYxNzg0ZTg1NDY2ZmJhYmQyNTc2OCIsImlhdCI6MTU4NTg0NjI0Mn0.vJ5pQfIUX8jGSodwiKhxI9pP5HLFiko7uHUSLWeXM2k')
 
-      const author1 = bson.ObjectID.createFromHexString(token._id);
-      const res1 = await User.createDraft(author1.toHexString(), draft, app.locals.userCollection, app.locals.tagCollection);
-      const user = new User({ _id: author1 });
-      const res2 = await user.publishDraft(res1.draftId, app.locals.userCollection, app.locals.blogCollection);
+      const author1 = bson.ObjectID.createFromHexString(token._id)
+      const res1 = await User.createDraft(author1.toHexString(), draft, app.locals.userCollection, app.locals.tagCollection)
+      const user = new User({ _id: author1 })
+      const res2 = await user.publishDraft(res1.draftId, app.locals.userCollection, app.locals.blogCollection)
       res = await chai.request(app)
         .post('/blogs/downvote')
         .set({
           Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFjaGhhcG9saWExMCIsIl9pZCI6IjVlODYxNzg0ZTg1NDY2ZmJhYmQyNTc2OCIsImlhdCI6MTU4NTg0NjI0Mn0.vJ5pQfIUX8jGSodwiKhxI9pP5HLFiko7uHUSLWeXM2k',
           'Content-Type': 'application/json'
         })
-        .send({ blogId: res2.blogId });
+        .send({ blogId: res2.blogId })
     } catch (e) {
-      assert.isNull(e);
+      assert.isNull(e)
     }
 
-    assert.equal(res.status, 200);
-  });
+    assert.equal(res.status, 200)
+  })
 
   it('POST /removeDownvote', async function () {
-    let res;
+    let res
     try {
-      const draft = { content: 'draft content', title: 'draft title', tags: ['#google', '#noob'] };
-      const token = jwt.decode('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFjaGhhcG9saWExMCIsIl9pZCI6IjVlODYxNzg0ZTg1NDY2ZmJhYmQyNTc2OCIsImlhdCI6MTU4NTg0NjI0Mn0.vJ5pQfIUX8jGSodwiKhxI9pP5HLFiko7uHUSLWeXM2k');
+      const draft = { content: 'draft content', title: 'draft title', tags: ['#google', '#noob'] }
+      const token = jwt.decode('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFjaGhhcG9saWExMCIsIl9pZCI6IjVlODYxNzg0ZTg1NDY2ZmJhYmQyNTc2OCIsImlhdCI6MTU4NTg0NjI0Mn0.vJ5pQfIUX8jGSodwiKhxI9pP5HLFiko7uHUSLWeXM2k')
 
-      const author1 = bson.ObjectID.createFromHexString(token._id);
-      const res1 = await User.createDraft(author1.toHexString(), draft, app.locals.userCollection, app.locals.tagCollection);
-      const user = new User({ _id: author1 });
-      const res2 = await user.publishDraft(res1.draftId, app.locals.userCollection, app.locals.blogCollection);
-      await Blog.addDownvote(res2.blogId, token._id, app.locals.blogCollection);
+      const author1 = bson.ObjectID.createFromHexString(token._id)
+      const res1 = await User.createDraft(author1.toHexString(), draft, app.locals.userCollection, app.locals.tagCollection)
+      const user = new User({ _id: author1 })
+      const res2 = await user.publishDraft(res1.draftId, app.locals.userCollection, app.locals.blogCollection)
+      await Blog.addDownvote(res2.blogId, token._id, app.locals.blogCollection)
       res = await chai.request(app)
         .post('/blogs/removeDownvote')
         .set({
           Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFjaGhhcG9saWExMCIsIl9pZCI6IjVlODYxNzg0ZTg1NDY2ZmJhYmQyNTc2OCIsImlhdCI6MTU4NTg0NjI0Mn0.vJ5pQfIUX8jGSodwiKhxI9pP5HLFiko7uHUSLWeXM2k',
           'Content-Type': 'application/json'
         })
-        .send({ blogId: res2.blogId });
+        .send({ blogId: res2.blogId })
     } catch (e) {
-      assert.isNull(e);
+      assert.isNull(e)
     }
 
-    assert.equal(res.status, 200);
-  });
+    assert.equal(res.status, 200)
+  })
 
   it('POST /removeUpvote', async function () {
-    let res;
+    let res
     try {
-      const draft = { content: 'draft content', title: 'draft title', tags: ['#google', '#noob'] };
-      const token = jwt.decode('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFjaGhhcG9saWExMCIsIl9pZCI6IjVlODYxNzg0ZTg1NDY2ZmJhYmQyNTc2OCIsImlhdCI6MTU4NTg0NjI0Mn0.vJ5pQfIUX8jGSodwiKhxI9pP5HLFiko7uHUSLWeXM2k');
+      const draft = { content: 'draft content', title: 'draft title', tags: ['#google', '#noob'] }
+      const token = jwt.decode('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFjaGhhcG9saWExMCIsIl9pZCI6IjVlODYxNzg0ZTg1NDY2ZmJhYmQyNTc2OCIsImlhdCI6MTU4NTg0NjI0Mn0.vJ5pQfIUX8jGSodwiKhxI9pP5HLFiko7uHUSLWeXM2k')
 
-      const author1 = bson.ObjectID.createFromHexString(token._id);
-      const res1 = await User.createDraft(author1.toHexString(), draft, app.locals.userCollection, app.locals.tagCollection);
-      const user = new User({ _id: author1 });
-      const res2 = await user.publishDraft(res1.draftId, app.locals.userCollection, app.locals.blogCollection);
-      await Blog.addUpvote(res2.blogId, token._id, app.locals.blogCollection);
+      const author1 = bson.ObjectID.createFromHexString(token._id)
+      const res1 = await User.createDraft(author1.toHexString(), draft, app.locals.userCollection, app.locals.tagCollection)
+      const user = new User({ _id: author1 })
+      const res2 = await user.publishDraft(res1.draftId, app.locals.userCollection, app.locals.blogCollection)
+      await Blog.addUpvote(res2.blogId, token._id, app.locals.blogCollection)
       res = await chai.request(app)
         .post('/blogs/removeUpvote')
         .set({
           Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFjaGhhcG9saWExMCIsIl9pZCI6IjVlODYxNzg0ZTg1NDY2ZmJhYmQyNTc2OCIsImlhdCI6MTU4NTg0NjI0Mn0.vJ5pQfIUX8jGSodwiKhxI9pP5HLFiko7uHUSLWeXM2k',
           'Content-Type': 'application/json'
         })
-        .send({ blogId: res2.blogId });
+        .send({ blogId: res2.blogId })
     } catch (e) {
-      assert.isNull(e);
+      assert.isNull(e)
     }
 
-    assert.equal(res.status, 200);
-  });
-});
+    assert.equal(res.status, 200)
+  })
+  it('GET /search', async function () {
+    const res = await chai.request(app)
+      .get(`/blogs/search?q='content'&ld=${Date.now()}`)
+    assert.isTrue(res.body.status)
+  })
+})
